05b8e312e3ac2c79c90801edea82858856ba3f18
AMIDIBOSS <adriandabski@gmail.com>
2017-01-29 13:56:07 +0100

    Added core files (Unfinished!)
    ^^

---
+++ ESOS/ESOS_core/Kernel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace ESOS_core
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.Clear();
+            Console.WriteLine("Starting ESOS...");
+            core.boot();
+        }
+
+        protected override void Run()
+        {
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+            Console.Write("Text typed: ");
+            Console.WriteLine(input);
+        }
+    }
+}
---
+++ ESOS/ESOS_core/core.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESOS_core
+{
+    
+    class core
+    {
+        //system build version and stability codes
+        static int sysver = 010;
+        static string sysversion = "0.1.0";
+        static bool isstable = false;
+        static int stabl = 0;
+        /*
+        A little note about stability levels:
+        0 = builded for testing purpouses, untested, build hourly, VERY unstable
+        1 = build for testing purpouses, tested for 1 min, build daily
+        2 = Snapshot
+    */
+        //system properties and configs
+
+        //system onBoot
+        internal static void boot()
+        {
+            Console.WriteLine("Version" + sysversion);
+            string stablString = stabl.ToString();
+            Console.WriteLine("System stablilty level = " + stablString + " (I'd be happy if you use at leats 4)");
+            if (isstable==false) { Console.WriteLine("WARNING!: This Build Is Unstable! Use at Your Own Risk!!!"); }
+            Console.WriteLine("");
+            Console.WriteLine("Boot complete, now launching console");
+
+
+        }
+        internal static void error(string error) {
+            Console.WriteLine("Woops, Looks like something just exploded:");
+            Console.WriteLine(error);
+            Kernel.BeforeRun();
+        }
+        internal static void console() {
+
+        }
+    }
+}
